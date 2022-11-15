fa6245b55ce012d429072ca9f73e5c0dfd887de2
smurd <shawn.s.murdzek@noaa.gov>
2022-11-15 18:43:57 +0000

    Create input files to create WRF nature run graphics

--- pre.sh
+++ pre.sh
@@ -6,6 +6,8 @@ module use -a /contrib/miniconda3/modulefiles
 module load miniconda3/4.5.12
 conda activate pygraf
 
-module load wgrib2/0.1.9.6a
+# SSM: Load intel and a different version of wgrib2
+module load intel/18.0.5.274
+module load wgrib2/2.0.8
 
 module list
---
+++ run_pygraf.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+#SBATCH -A wrfruc
+#SBATCH -t 02:00:00
+#SBATCH --ntasks=4
+
+date
+
+cd /scratch1/BMC/wrfruc/murdzek/pygraf/
+
+source ./pre.sh
+
+python create_graphics.py \
+         maps \
+         --all_leads \
+         -d /scratch1/BMC/wrfruc/murdzek/nature_run_3km/WRF/run \
+         -f 0 \
+         --file_tmpl "wrfnat_hrconus_{FCST_TIME:02d}.grib2" \
+         --file_type nat \
+         --images ./image_lists/wrfnat.yml hourly \
+         -m "Nature Run" \
+         -n 4 \
+         -o /scratch1/BMC/wrfruc/murdzek/nature_run_3km/graphics \
+         -s 2021072423 \
+         --tiles full
+
+python create_graphics.py \
+         maps \
+         --all_leads \
+         -d /scratch1/BMC/wrfruc/murdzek/nature_run_3km/WRF/run \
+         -f 0 \
+         --file_tmpl "wrfprs_hrconus_{FCST_TIME:02d}.grib2" \
+         --file_type prs \
+         --images ./image_lists/wrfprs.yml hourly \
+         -m "Nature Run" \
+         -n 4 \
+         -o /scratch1/BMC/wrfruc/murdzek/nature_run_3km/graphics \
+         -s 2021072423 \
+         --tiles full
+
+report-mem
+date
